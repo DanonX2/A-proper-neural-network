45b8a9c5ecd3bebd92af7d8f5cd7a6a52c3eab78
samuel <chensrj2@gmail.com>
2019-01-19 15:52:55 -0600

    adsfds

--- get_d.py
+++ get_d.py
@@ -1,2 +1,3 @@
-x = range(0,4)
-print(type(x))
+x = [1,2,3,4]
+y = [2,3,4,5]
+print(x + y)
--- package.py
+++ package.py
@@ -46,7 +46,7 @@ class network():
         self.output = ['initalization required!']  #above: add a layer with the privious layer as the inputlayer and numofnueron based on dimension
         self.gradients = [[] for i in range(len(self.dimension))]
         for j in range(len(self.dimension)):
-            self.gradients[j] = [[] for i in range(self.dimension[j])]
+            self.gradients[j] = [[[] for i in range(2)] for i in range(self.dimension[j])]
     def forwardpropagation(self):
         for i in self.layers:
             if i==self.layers[0]:pass
@@ -57,26 +57,44 @@ class network():
         for i in range(len(target)):
             self.cost += (target[i]-self.output[i]) ** 2
         return self.cost
-    def get_gradients(self):
-        for l in self.layers[1:]:
-            for n in l.neurons:
-                if sum(n.input) * n.weight + n.bias > 0:
-                    self.gradients[l][n] = sum(n.input)
-                else:
-                    self.gradients[l][n] = 0
+    def setdata(self,training,testing,valadating):
+        self.trainingdata = training
+        self.testingdata = testing
+        self.valadatingdata = valadating
+    def get_gradients(self,flashtarget):
         self.ata = [[] for i in range(len(self.dimension)-1)]
         for i in range(len(self.layers)-2):
-            self.ata[i] = [sum(j.weight for j in self.layers[i+2].neurons)]
+            self.ata[i] = [j.weight for j in self.layers[i+2].neurons]
         for l in range(len(self.gradients)):
-            for n in range(len(self.gradients[l].neurons)):
-                if (sum(self.layers[l+1].neurons[n].input) * self.layers[l+1].neurons[n+1].weight + self.layers[l+1].neurons[n+1].bias) > 0:
-                    #self.gradients[l][n] *= 
-                    pass
-            
+            for n in range(len(self.gradients[l])):
+                if (sum(self.layers[l+1].neurons[n].input) * self.layers[l+1].neurons[n].weight + self.layers[l+1].neurons[n].bias) < 0:
+                    del self.ata[l][n]
+        for i in range(len(self.ata)):self.ata[i] = sum(self.ata[i])
+        for i in range(len(self.ata)):
+            for j in self.ata[i:]:
+                self.ata[i] *= j
+        for l in range(len(self.layers[1:-1])):
+            for n in range(len(self.gradients[l])):
+                if sum(self.layers[l+1].neurons[n].input) * self.layers[l+1].neurons[n].weight + self.layers[l+1].neurons[n].bias > 0:
+                    self.gradients[l][n][0] = sum(self.layers[l+1].neurons[n].input) * self.ata[l] * (-2 * (sum(flashtarget) - sum(self.layers[-1].outputlayer)))
+                    self.gradients[l][n][1] = 1 * self.ata[l] * (-2 * (sum(flashtarget) - sum(self.layers[-1].outputlayer)))
+                else:
+                    self.gradients[l][n][0] = 0
+                    self.gradients[l][n][1] = 0
+        for i in self.gradients[-1]:
+            i[0] = sum(self.layers[l+1].neurons[n].input) * (-2 * (sum(flashtarget) - sum(self.layers[-1].outputlayer)))
+            i[1] = 1 * (-2 * (sum(flashtarget) - sum(self.layers[-1].outputlayer)))
+    def train(self,rate,rounds):
+        for i in range(rounds):
+            self.forwardpropagation()
+            self.get_gradients(self.trainingdata[i][1])
+            #backpropagation
+            for l in range(len(self.layers[1:])):
+                for n in range(len(self.layers[l].neurons)):
+                    self.layers[l].neurons[n] *= -self.gradients[l][n] * rate
+
 
 
 x = network([1],[1,2,1])
-x.forwardpropagation()
-print(x.output)
-x.get_gradients()
-print(x.ata)
+x.get_gradients([1])
+print(x.gradients)
